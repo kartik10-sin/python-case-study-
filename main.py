3f82044984f18d61e25216311ac33fc63384dbad
kartik10-sin <kartiksingh94124@gmail.com>
2026-02-25 23:00:58 +0530

    Update guess.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import random
 
-# -----------------------
-# Number Guessing Game
-# -----------------------
-
 n = random.randint(1, 100)
 
 print("Number Guessing Game")
@@ -20,27 +16,3 @@ while True:
     else:
         print("Too high")
 
-
-# -----------------------
-# Word Scrambler Game
-# -----------------------
-
-words = ["python", "jupyter", "colab", "programming", "scramble", "developer"]
-
-selected_word = random.choice(words)
-word_list = list(selected_word)
-
-random.shuffle(word_list)
-scrambled = ''.join(word_list)
-
-print("\nWelcome to the Word Scrambler Game!")
-print(f"Unscramble the word: {scrambled}")
-
-while True:
-    guess = input("Your guess: ").lower()
-
-    if guess == selected_word:
-        print(f"Congratulations! You unscrambled the word '{selected_word}'.")
-        break
-    else:
-        print("Incorrect. Try again!")
